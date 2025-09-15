1b3cd0d2b2625ddab27281612b55fa702d52f2d4
asqer92 <asqer92@u.sogang.ac.kr>
2025-09-15 19:57:35 +0900

    refactor: remove unused ojt function and clean up code

--- 20250915.py
+++ 20250915.py
@@ -1,3 +1,25 @@
+# def ojt():
+#     import math
+    
+#     n1, n2 = 10, 34.1
+#     print('n1 =', n1, 'n2 =', n2)
+#     n1, n2 = n2, n1
+#     print('n1 =', n1, 'n2 =', n2)
+#     n1 = int(n1)
+#     n2 = float(n2)
+#     print('n1 =', n1, 'n2 =', n2)
+#     print('Result:' + str(n1) + "+" + str(n2))
+#     print()
+#     a = 100
+#     print(a%2 == 0)
+#     print(a%3 == 0)
+#     print(0.1+0.1+0.1 == 0.3)
+#     print(round(0.1+0.1+0.1, 10) == 0.3)
+#     print("################################")
+#     num=81
+#     print(num**0.5 == math.pow(num, 0.5))
+#     print(math.pow(num, 0.5)  == math.sqrt(num))
+
 def hundred_digit_after_five_times():
     import random
 
@@ -11,28 +33,6 @@ def hundred_digit_after_five_times():
 
     print("Hundred-digit number after five times is", hundreds)
 
-
-def ojt():
-    import math
-    
-    n1, n2 = 10, 34.1
-    print('n1 =', n1, 'n2 =', n2)
-    n1, n2 = n2, n1
-    print('n1 =', n1, 'n2 =', n2)
-    n1 = int(n1)
-    n2 = float(n2)
-    print('n1 =', n1, 'n2 =', n2)
-    print('Result:' + str(n1) + "+" + str(n2))
-    print()
-    a = 100
-    print(a%2 == 0)
-    print(a%3 == 0)
-    print(0.1+0.1+0.1 == 0.3)
-    print(round(0.1+0.1+0.1, 10) == 0.3)
-    print("################################")
-    num=81
-    print(num**0.5 == math.pow(num, 0.5))
-    print(math.pow(num, 0.5)  == math.sqrt(num))
 
 def main():
     hundred_digit_after_five_times()
