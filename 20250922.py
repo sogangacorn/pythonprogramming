6084ccc53b792caf0c85339d2435aeab31ce3638
asqer92 <asqer92@u.sogang.ac.kr>
2025-09-22 19:47:43 +0900

    feat: implement student score input and calculation functionality

--- 20250922.py
+++ 20250922.py
@@ -1,5 +1,18 @@
-data = 'kim 34'
-name, score = data.split(" ")
-print(name)
-print(score)
-print(int(score) + 6)
+name_and_scores = input("Enter the names and scores of 3 students :")
+delimeter = input('Enter characters to split :')
+
+s1, s2, s3 = name_and_scores.split(delimeter)
+s1_name, s1_score = s1.split()
+s2_name, s2_score = s2.split()
+s3_name, s3_score = s3.split()
+
+s1_score = int(s1_score)
+s2_score = int(s2_score)
+s3_score = int(s3_score)
+
+print("Student1 name : %s, score : %d" % (s1_name, s1_score))
+print("Student2 name : %s, score : %d" % (s2_name, s2_score))
+print("Student3 name : %s, score : %d" % (s3_name, s3_score))
+
+print("Total score : %d," % (s1_score + s2_score + s3_score), 
+      "Average score : %.2f" % ((s1_score + s2_score + s3_score) / 3))
